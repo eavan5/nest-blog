36fc65db01bac2f93cb520b67ccc98ac5a904f70
wumao <webwwy@gmail.com>
2021-01-02 11:42:16 +0800

    feat:链接mongodb

---
+++ src/app.config.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: wumao
+ * @Date: 2021-01-02 11:17:07
+ * @LastEditTime: 2021-01-02 11:40:27
+ * @LastEditors: Please set LastEditors
+ * @Description: 默认的配置文件
+ * @FilePath: /nest-blog/src/app.config.ts
+ */
+export const CONFIG_G = {
+  PORT: 3000,
+};
+export const MONGO_DB = {
+  uri: `mongodb://127.0.0.1:27017/nestBlog`,
+  username: 'admin',
+  password: '123456',
+  url: 'mongodb://admin:123456@localhost:27017/blog?authSource=admin',
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-02 10:59:57
+ * @LastEditTime: 2021-01-02 11:40:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /nest-blog/src/app.module.ts
+ */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MONGO_DB } from './app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(MONGO_DB.url, {
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
